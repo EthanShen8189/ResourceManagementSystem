12d9bf17ac58201a79bc9a19ced5e495a5589bd3
EthanShen8189 <sy8189@gmail.com>
2016-12-02 18:44:49 -0500

    Dao & Model examples

--- src/main/java/concordia/soen387/project/Dao/ReservationDaoImpl.java
+++ src/main/java/concordia/soen387/project/Dao/ReservationDaoImpl.java
@@ -1,7 +1,20 @@
 package concordia.soen387.project.Dao;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
 /**
  * Created by EthanShen on 2016-12-02.
  */
 public class ReservationDaoImpl implements ReservationDao{
+
+    private JdbcTemplate jdbcTemplate;
+
+    public ReservationDaoImpl(){}
+
+    public ReservationDaoImpl(DataSource dataSource){
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
 }
