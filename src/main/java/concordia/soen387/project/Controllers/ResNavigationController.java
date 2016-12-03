d6fd10a14fa5502ff9da3c4eb3910adb04316a49
Van Do <Van_do@live.com>
2016-12-02 19:50:40 -0500

    renamed NavigationController to InvNavigationController.  Changed the indexes to the proper index based on inventory or reservation

---
+++ src/main/java/concordia/soen387/project/Controllers/ResNavigationController.java
@@ -0,0 +1,52 @@
+package concordia.soen387.project.Controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+public class ResNavigationController {
+	
+	private ModelAndView modelAndView = new ModelAndView();
+	//private ResNavigationBarService resNavigationBarService = new ResNavigationBarService();
+	
+	//accountSettings
+	@RequestMapping(value = "/accountSettingsTab")
+	public ModelAndView accountSettingsNav(){
+		modelAndView.setViewName("resIndex");
+		modelAndView.addObject("accountSettingsTabActive", "active");
+		modelAndView.addObject("reservationsTabActive","");
+		modelAndView.addObject("addReservationTabActive", "");
+		modelAndView.addObject("selectedTab", "../jsp/inventory/accountSettings.jsp");
+		return modelAndView;
+		
+	}
+	
+	//reservationsTab
+	@RequestMapping(value = "/reservationsTab")
+	public ModelAndView reservationsNav(){
+		modelAndView.setViewName("resIndex");
+		modelAndView.addObject("accountSettingsTabActive", "");
+		modelAndView.addObject("reservationsTabActive","active");
+		modelAndView.addObject("addReservationTabActive", "");
+		modelAndView.addObject("selectedTab", "../jsp/reservation/my-reservations.jsp");
+		return modelAndView;
+		
+	}
+	
+	
+	//addreservationsTab
+	@RequestMapping(value = "/addReservationTab")
+	public ModelAndView addReservationNav(){
+		modelAndView.setViewName("resIndex");
+		modelAndView.addObject("accountSettingsTabActive", "");
+		modelAndView.addObject("reservationsTabActive","");
+		modelAndView.addObject("addReservationTabActive", "active");
+		modelAndView.addObject("selectedTab", "../jsp/inventory/search.jsp");
+		return modelAndView;
+		
+	}
+	
+	
+	
+
+
+}
